2a6f3a6aa59bc4892488b153f472713ca2fa57ab
Victor Romero <vromerocomino@gmail.com>
2022-05-17 08:56:14 +0200

    fix(my-list): fix rendering when list is null

--- src/components/my-list.component.js
+++ src/components/my-list.component.js
@@ -35,7 +35,7 @@ export class MyListComponent extends LitElement {
         <section>
           <h1>Películas</h1>
           ${
-            this.movies.length
+            this.movies && this.movies.length
             ? html`<tmdb-cards-list-ui .list=${this.movies} .genres=${this.genres} landscape></tmdb-cards-list-ui>`
             : html`<p>No hay ninguna película guardada todavía.</p>`
           }
@@ -43,7 +43,7 @@ export class MyListComponent extends LitElement {
         <section>
           <h1>Programas de TV y series</h1>
           ${
-            this.tv.length
+            this.tv && this.tv.length
             ? html`<tmdb-cards-list-ui .list=${this.tv} .genres=${this.genres} landscape></tmdb-cards-list-ui>`
             : html`<p>No hay ningun programa de TV o serie guardada todavía.</p>`
           }
